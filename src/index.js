5ee827fbe485b7ec8d8821508f7e6091246585e1
borozky <borozky@gmail.com>
2018-01-11 18:31:09 +1100

    Added delete keyboard functionality

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import $ from "jquery";
 import "jquery-ui-bundle";
 import ruler from "./scripts/ruler";
 import {app} from "./App";
+import Mousetrap from 'mousetrap';
 import "./styles/index.css";
+import store from './scripts/store';
 
 
 window.jQuery = $;
@@ -24,6 +26,13 @@ $(function(){
     window.myRuler = myRuler;
     window.appCanvas = app.AppCanvas;
     document.body.setAttribute("class", "loaded");
+
+    Mousetrap.bind('del', function() {
+        if (document.activeElement === document.body) {
+            store.dispatch({ type: "REMOVE_SELECTED_ITEMS" });
+        }
+    });
+
 });
 
 
